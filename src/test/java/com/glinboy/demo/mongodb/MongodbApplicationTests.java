4df313a82229d4f68694b235cbf88b31cb86ad29
GLinBoy <glinboy@live.com>
2024-09-18 19:30:44 +0200

    🕯 START: Initiated the project
    resoved #1

---
+++ src/test/java/com/glinboy/demo/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.demo.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
