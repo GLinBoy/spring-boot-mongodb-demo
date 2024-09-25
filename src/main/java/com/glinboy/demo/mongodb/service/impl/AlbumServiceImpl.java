2905cb612ba8cc3c147a2a5d5247f8ab91b0b646
GLinBoy <glinboy@live.com>
2024-09-25 18:12:24 +0200

    📦 NEW: Add Album service impl.

---
+++ src/main/java/com/glinboy/demo/mongodb/service/impl/AlbumServiceImpl.java
@@ -0,0 +1,10 @@
+package com.glinboy.demo.mongodb.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.glinboy.demo.mongodb.service.AlbumService;
+
+@Service
+public class AlbumServiceImpl implements AlbumService {
+
+}
