687501b046b7bfa01081d72886f751fd25ce948d
GLinBoy <glinboy@live.com>
2024-09-25 18:12:53 +0200

    📦 NEW: Add Album resource

---
+++ src/main/java/com/glinboy/demo/mongodb/web/rest/AlbumResource.java
@@ -0,0 +1,13 @@
+package com.glinboy.demo.mongodb.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/albums")
+public class AlbumResource {
+
+}
