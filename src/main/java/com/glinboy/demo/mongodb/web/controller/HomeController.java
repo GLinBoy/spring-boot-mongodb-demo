8ad34491443599a0c0e1c0cb8c6b093cb79c1da5
GLinBoy <glinboy@live.com>
2024-09-25 18:45:11 +0200

    📦 NEW: Add Home controller and redirect root directory to swagger

---
+++ src/main/java/com/glinboy/demo/mongodb/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.glinboy.demo.mongodb.web.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public void redirect(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/swagger-ui.html");
+	}
+
+}
