dd32f43de4a0f619962276a85d261a4fcd6daf65
GLinBoy <glinboy@live.com>
2024-09-28 16:11:32 +0200

    👌 IMPROVE: Add an API to ArtistService to get all ArtistDTO

--- src/main/java/com/glinboy/demo/mongodb/service/ArtistService.java
+++ src/main/java/com/glinboy/demo/mongodb/service/ArtistService.java
@@ -1,5 +1,12 @@
 package com.glinboy.demo.mongodb.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.glinboy.demo.mongodb.service.dto.ArtistDTO;
+
 public interface ArtistService {
+
+	Page<ArtistDTO> getAllArtist(Pageable pageable);
 
 }
