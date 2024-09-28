4cc9aeaba4810cf3648dfccdb9b31fc318e488ed
GLinBoy <glinboy@live.com>
2024-09-28 16:12:35 +0200

    👌 IMPROVE: Expose a method to get all Artist

--- src/main/java/com/glinboy/demo/mongodb/web/rest/ArtistResource.java
+++ src/main/java/com/glinboy/demo/mongodb/web/rest/ArtistResource.java
@@ -1,8 +1,18 @@
 package com.glinboy.demo.mongodb.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.glinboy.demo.mongodb.service.ArtistService;
+import com.glinboy.demo.mongodb.service.dto.ArtistDTO;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -10,4 +20,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/artists")
 public class ArtistResource {
 
+	private final ArtistService artistService;
+
+	@GetMapping
+	public ResponseEntity<Page<ArtistDTO>> getAllArtist(@Parameter(hidden = true) Pageable pageable) {
+		return ResponseEntity.ok(artistService.getAllArtist(pageable));
+	}
 }
