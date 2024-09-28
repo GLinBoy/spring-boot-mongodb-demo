46c7e34380918ac0a69c371f233e361b09617142
GLinBoy <glinboy@live.com>
2024-09-28 16:11:54 +0200

    👌 IMPROVE: Add an impl. of get all ArtistDTO

--- src/main/java/com/glinboy/demo/mongodb/service/impl/ArtistServiceImpl.java
+++ src/main/java/com/glinboy/demo/mongodb/service/impl/ArtistServiceImpl.java
@@ -1,10 +1,29 @@
 package com.glinboy.demo.mongodb.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.glinboy.demo.mongodb.entity.Artist;
+import com.glinboy.demo.mongodb.repository.ArtistRepository;
 import com.glinboy.demo.mongodb.service.ArtistService;
+import com.glinboy.demo.mongodb.service.dto.ArtistDTO;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class ArtistServiceImpl implements ArtistService {
 
+	private final ArtistRepository artistRepository;
+	private final ModelMapper mapper;
+
+	@Override
+	public Page<ArtistDTO> getAllArtist(Pageable pageable) {
+		return artistRepository.findAll(pageable)
+				.map(e -> mapper.map(e, ArtistDTO.class));
+	}
 }
