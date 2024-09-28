82c929aeeeaff0801d79c2aeb56572bc8118b234
GLinBoy <glinboy@live.com>
2024-09-28 16:08:51 +0200

    👌 IMPROVE: Update AlbumDTO record to class

--- src/main/java/com/glinboy/demo/mongodb/service/dto/AlbumDTO.java
+++ src/main/java/com/glinboy/demo/mongodb/service/dto/AlbumDTO.java
@@ -1,14 +1,21 @@
 package com.glinboy.demo.mongodb.service.dto;
 
-public record AlbumDTO(
-		String id,
-		Long artistId,
-		String albumTitle,
-		String genre,
-		String yearOfPub,
-		Short numOfTracks,
-		Long numOfSales,
-		Float rollingStoneCritic,
-		Float mtvCritic,
-		Float musicManiacCritic
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlbumDTO {
+	private String id;
+	private Long artistId;
+	private String albumTitle;
+	private String genre;
+	private String yearOfPub;
+	private Short numOfTracks;
+	private Long numOfSales;
+	private Float rollingStoneCritic;
+	private Float mtvCritic;
+	private Float musicManiacCritic;
+}
