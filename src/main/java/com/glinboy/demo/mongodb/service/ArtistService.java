08a3fd3bb5203976191528f77f31a9dce56f7352
GLinBoy <glinboy@live.com>
2024-09-29 11:34:43 +0200

    👌 IMPROVE: Add a method to get an Artist

--- src/main/java/com/glinboy/demo/mongodb/service/ArtistService.java
+++ src/main/java/com/glinboy/demo/mongodb/service/ArtistService.java
@@ -1,5 +1,7 @@
 package com.glinboy.demo.mongodb.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,6 +10,8 @@ import com.glinboy.demo.mongodb.service.dto.ArtistDTO;
 public interface ArtistService {
 
 	Page<ArtistDTO> getAllArtist(Pageable pageable);
+	
+	Optional<ArtistDTO> getArtist(String id);
 
 	ArtistDTO save(ArtistDTO artistDTO);
 
