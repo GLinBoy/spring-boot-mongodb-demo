4c07ed9fb7e4249466e58a888625fc583c04d703
GLinBoy <glinboy@live.com>
2024-09-30 19:48:54 +0200

    👌 IMPROVE: Add a method impl. to get all Albums

--- src/main/java/com/glinboy/demo/mongodb/service/impl/AlbumServiceImpl.java
+++ src/main/java/com/glinboy/demo/mongodb/service/impl/AlbumServiceImpl.java
@@ -1,10 +1,27 @@
 package com.glinboy.demo.mongodb.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.glinboy.demo.mongodb.repository.AlbumRepository;
 import com.glinboy.demo.mongodb.service.AlbumService;
+import com.glinboy.demo.mongodb.service.dto.AlbumDTO;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class AlbumServiceImpl implements AlbumService {
+	
+	private final AlbumRepository albumRepository;
+	private final ModelMapper mapper;
+	
+	@Override
+	public Page<AlbumDTO> getAllAlbums(Pageable pageable) {
+		return albumRepository.findAll(pageable)
+				.map(e -> mapper.map(e, AlbumDTO.class));
+	}
 
 }
