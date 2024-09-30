6a7d8706cea49b802600862ee4138ad82a8574a0
GLinBoy <glinboy@live.com>
2024-09-30 19:40:33 +0200

    👌 IMPROVE: Add a method API to get an Album

--- src/main/java/com/glinboy/demo/mongodb/service/AlbumService.java
+++ src/main/java/com/glinboy/demo/mongodb/service/AlbumService.java
@@ -1,5 +1,7 @@
 package com.glinboy.demo.mongodb.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,5 +10,7 @@ import com.glinboy.demo.mongodb.service.dto.AlbumDTO;
 public interface AlbumService {
 	
 	Page<AlbumDTO> getAllAlbums(Pageable pageable);
+	
+	Optional<AlbumDTO> getAlbum(String id);
 
 }
