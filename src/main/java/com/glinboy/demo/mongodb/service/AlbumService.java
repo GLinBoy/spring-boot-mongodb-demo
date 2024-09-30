79a3f3b2bcd232676366579c89837efdbaa9c983
GLinBoy <glinboy@live.com>
2024-09-30 19:38:37 +0200

    👌 IMPROVE: Add a method to get all Albums

--- src/main/java/com/glinboy/demo/mongodb/service/AlbumService.java
+++ src/main/java/com/glinboy/demo/mongodb/service/AlbumService.java
@@ -1,5 +1,12 @@
 package com.glinboy.demo.mongodb.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.glinboy.demo.mongodb.service.dto.AlbumDTO;
+
 public interface AlbumService {
+	
+	Page<AlbumDTO> getAllAlbums(Pageable pageable);
 
 }
