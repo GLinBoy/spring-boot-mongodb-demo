65b08ee997164dcc12f53cf865167dbea6edd54e
GLinBoy <glinboy@live.com>
2024-10-15 20:32:19 +0200

    👌 IMPROVE: Expose a method to get all Album

--- src/main/java/com/glinboy/demo/mongodb/web/rest/AlbumResource.java
+++ src/main/java/com/glinboy/demo/mongodb/web/rest/AlbumResource.java
@@ -1,5 +1,13 @@
 package com.glinboy.demo.mongodb.web.rest;
 
+import com.glinboy.demo.mongodb.service.AlbumService;
+import com.glinboy.demo.mongodb.service.dto.AlbumDTO;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springdoc.core.converters.models.PageableAsQueryParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/albums")
 public class AlbumResource {
+
+	private final AlbumService albumService;
+
+	@GetMapping
+	@PageableAsQueryParam
+	public ResponseEntity<Page<AlbumDTO>> getAllAlbum(@Parameter(hidden = true) Pageable pageable) {
+		return ResponseEntity.ok(albumService.getAllAlbums(pageable));
+	}
 
 }
