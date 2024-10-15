cc6baa25687cedee21d02d8330dd11430c141b2b
GLinBoy <glinboy@live.com>
2024-10-15 20:41:46 +0200

    👌 IMPROVE: Expose a method to save an Album

--- src/main/java/com/glinboy/demo/mongodb/web/rest/AlbumResource.java
+++ src/main/java/com/glinboy/demo/mongodb/web/rest/AlbumResource.java
@@ -3,16 +3,16 @@ package com.glinboy.demo.mongodb.web.rest;
 import com.glinboy.demo.mongodb.service.AlbumService;
 import com.glinboy.demo.mongodb.service.dto.AlbumDTO;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +32,16 @@ public class AlbumResource {
 		return albumService.getAlbum(id)
 			.map(ResponseEntity::ok)
 			.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<AlbumDTO> saveAlbum(@RequestBody AlbumDTO albumDTO,
+																						HttpServletRequest request) {
+		AlbumDTO savedAlbumDTO = albumService.saveAlbum(albumDTO);
+		URI location = URI.create(String.format("%s/%s", request.getRequestURI(), savedAlbumDTO.getId()));
+		return ResponseEntity.created(location)
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(savedAlbumDTO);
 	}
 
 }
